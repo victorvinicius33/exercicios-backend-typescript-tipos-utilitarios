6591db0aede3982746363da210afbd9cd74cf24d
Victor Dell <victorvinicius33.vv@gmail.com>
2024-07-29 18:23:11 -0300

    03 done

--- 03/scripts.ts
+++ 03/scripts.ts
@@ -6,7 +6,12 @@ type Conn = {
     dbname: string
 }
 
-const conexao = (dados: Conn) => {
+type Conexao = {
+    driver: string,
+    url: string
+}
+
+const conexao = (dados: Conn): Readonly<Conexao> => {
     const { username, password, host, port, dbname } = dados;
     return {
         driver: 'postgres',
